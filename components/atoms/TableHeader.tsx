16d2dd8d9221930e82bc9d70c147a75e7054754f
fitrahidayaat <fitrahidayaat@gmail.com>
2025-07-13 15:09:38 +0700

    [FEATURE] change the font weight of history header table to bold

--- components/atoms/TableHeader.tsx
+++ components/atoms/TableHeader.tsx
@@ -4,7 +4,7 @@ interface TableHeaderProps {
 
 export default function TableHeader({ children }: TableHeaderProps) {
   return (
-    <th className="px-6 py-3 text-left text-sm font-medium text-gray-700 border-b border-gray-200">
+    <th className="px-6 py-3 text-left text-sm font-bold text-gray-700 border-b border-gray-300">
       {children}
     </th>
   );
